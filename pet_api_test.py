91c2d82ceccd462ed6c9202fefc017a0abf91a0f
Azhara0205 <azhar.tursunova2001@gmail.com>
2024-09-01 16:31:40 +0200

    Update pet_api_test.py

--- pet_api_test.py
+++ pet_api_test.py
@@ -53,8 +53,9 @@ def test_pet_operations():
 
     update_pet = requests.put(f'{base_url}/pet/{pet_id}', json = update_data)
     print("Update pet response: " + update_pet.text)
-
-
+    
+    get_pet = requests.get(f'{base_url}/pet/{pet_id}')
+    print("Get pet response: " + get_pet.text)
 
 
     schema = {
